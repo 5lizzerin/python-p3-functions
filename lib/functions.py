03cfc7c91e0bcaf7db773ef661f82810cf8f0dd9
Lizz <lizzcklein@gmail.com>
2023-10-24 09:30:33 -0400

    tests passed

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,25 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
+greet_programmer()
+
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
+greet("Naureen")
+
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+greet_with_default()
+greet_with_default("Sunny")
+
+
 
 def add(num1, num2):
-    pass
+    print(num1 + num2)
+add(1, 1)
 
 def halve(number):
     pass
